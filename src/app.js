a32083983c5ad05853d4b6fff054cadcbde02cd0
Dmitry Osin <d@osin.pro>
2024-05-04 02:56:22 +0300

    [feature] Add passport.js with local strategy (#20)

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-process.env["NODE_CONFIG_DIR"] = __dirname + "/config/"
+process.env['NODE_CONFIG_DIR'] = __dirname + '/config/'
 
-const express = require("express")
+const express = require('express')
 const app = express()
 const session = require('express-session')
 
-const path = require("path")
+const path = require('path')
 const config = require('config')
 
-const passport = require("passport")
-const port = process.env.PORT || config.get("server.port") || 3000
+const passport = require('passport')
+const port = process.env.PORT || config.get('server.port') || 3000
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(passport.initialize())
 app.use(session({
-    secret: config.get("server.sessionSecret"),
+    secret: config.get('server.sessionSecret'),
     resave: false,
     saveUninitialized: false
 }))
 app.use(passport.authenticate('session'))
 
-app.set("views engine", "pug")
+app.set('views engine', 'pug')
 app.set('views', path.join(__dirname, 'view'))
 
 const router = require('./controller');
